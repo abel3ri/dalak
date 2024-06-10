f4faf2a053c43fbbfbbcba77aa129c69793dc637
abel3ri <abelmerete@yahoo.com>
2024-06-10 22:40:50 +0300

    add: app theme provider

---
+++ lib/providers/ThemeProvider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider with ChangeNotifier {
+  SharedPreferences prefs;
+  ThemeProvider({
+    required this.prefs,
+  });
+
+  String get theme {
+    return prefs.getString("theme") ?? "system";
+  }
+
+  void changeThemeMode(String themeMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("theme", themeMode);
+    notifyListeners();
+  }
+}
