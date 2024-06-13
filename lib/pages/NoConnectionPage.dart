f1b3a698f03b3dd1380c0f3d97dce7e7d98bcee4
abel3ri <abelmerete@yahoo.com>
2024-06-13 13:12:50 +0300

    add no connection page

---
+++ lib/pages/NoConnectionPage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class NoConnectionPage extends StatelessWidget {
+  const NoConnectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.wifi_off, size: 96),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+            Text(
+              "No Internet Connection",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    fontWeight: FontWeight.w500,
+                  ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+            Text(
+              "Check your Internet connection",
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+            FilledButton(
+                onPressed: () {
+                  GoRouter.of(context).pushReplacementNamed("splashPage");
+                },
+                child: Text("Try again"))
+          ],
+        ),
+      ),
+    );
+  }
+}
