bd34e7fdcd125833cf3aa693a4948046ddc88b64
abel3ri <abelmerete@yahoo.com>
2024-06-13 18:13:23 +0300

    modify: finish login page

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,12 +1,17 @@
-import 'package:dalak/widgets/CustomAppBar.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:dalak/controllers/FormValidator.dart';
+import 'package:dalak/providers/LoginFormProvider.dart';
+import 'package:dalak/widgets/CustomAppBar.dart';
+import 'package:dalak/widgets/LoginSignupInput.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final loginProvider = Provider.of<LoginFormProvider>(context);
     return Scaffold(
       appBar: CustomAppBar(
         leading: IconButton(
@@ -21,37 +26,103 @@ class LoginPage extends StatelessWidget {
           vertical: MediaQuery.of(context).size.height * 0.03,
           horizontal: MediaQuery.of(context).size.width * 0.06,
         ),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Login",
-                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                      fontWeight: FontWeight.w500,
-                    ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.02,
-              ),
-              Text(
-                "Login to access all features",
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      fontWeight: FontWeight.w500,
-                    ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.04,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  fillColor: Colors.grey.shade300,
-                  border: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Form(
+            key: loginProvider.formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "Login",
+                  style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                        fontWeight: FontWeight.w500,
+                      ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                Text(
+                  "Login to access all features",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w500,
+                      ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.04,
+                ),
+                FormInputField(
+                  controller: loginProvider.usernameEmailController,
+                  labelText: "Username or Email",
+                  hintText: "Enter your username or email",
+                  prefixIcon: Icons.person,
+                  keyBoardType: TextInputType.text,
+                  textInputAction: TextInputAction.next,
+                  validator: FormValidator.emailValidator,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                FormInputField(
+                  controller: loginProvider.passwordController,
+                  labelText: "Password",
+                  hintText: "Enter your password",
+                  prefixIcon: Icons.lock,
+                  sufficIcon: loginProvider.showPassword
+                      ? Icons.visibility_off
+                      : Icons.visibility,
+                  keyBoardType: TextInputType.visiblePassword,
+                  obscureText: loginProvider.showPassword,
+                  onSuffixIconTap: loginProvider.toggleShowPassword,
+                  textInputAction: TextInputAction.done,
+                  validator: FormValidator.passwordValidator,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.04,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Forgot password?",
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            fontWeight: FontWeight.w500,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                    ),
+                  ),
+                ),
+                FilledButton(
+                  onPressed: () {
+                    if (loginProvider.formKey.currentState!.validate()) {}
+                  },
+                  style: ButtonStyle(),
+                  child: Text("Login"),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?"),
+                    TextButton(
+                      onPressed: () {
+                        GoRouter.of(context).pushReplacementNamed("signup");
+                      },
+                      child: Text(
+                        "Create account",
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
