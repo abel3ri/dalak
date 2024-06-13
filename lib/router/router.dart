18a7a99d2f3f466faec6a1777dd42325da4c0ecc
abel3ri <abelmerete@yahoo.com>
2024-06-13 13:14:26 +0300

    modify: add pages to routes array

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,20 +1,56 @@
-import 'package:dalak/pages/HomePage.dart';
-import 'package:dalak/pages/SplashPage.dart';
+import 'package:dalak/pages/NoConnectionPage.dart';
+import 'package:dalak/pages/OnBoardingPage.dart';
 import 'package:go_router/go_router.dart';
+
+import 'package:dalak/pages/HomePage.dart';
+import 'package:dalak/pages/LoginPage.dart';
+import 'package:dalak/pages/SplashPage.dart';
+import 'package:dalak/utils/SlideTransition.dart';
 
 class AppRouter {
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: "/",
-        name: 'homePage',
-        builder: (context, state) => HomePage(),
+        name: 'splashPage',
+        pageBuilder: (context, state) => SlideTransitionPage(
+          key: state.pageKey,
+          child: SplashPage(),
+        ),
       ),
       GoRoute(
-        path: "/splash",
-        name: 'splashPage',
-        builder: (context, state) => SplashPage(),
-      )
+        path: "/home",
+        name: 'homePage',
+        pageBuilder: (context, state) => SlideTransitionPage(
+          key: state.pageKey,
+          child: HomePage(),
+        ),
+      ),
+      GoRoute(
+        path: "/login",
+        name: "login",
+        pageBuilder: (context, state) => SlideTransitionPage(
+          key: state.pageKey,
+          rtl: false,
+          child: LoginPage(),
+        ),
+      ),
+      GoRoute(
+        path: "/on-boarding",
+        name: "onBoarding",
+        pageBuilder: (context, state) => SlideTransitionPage(
+          key: state.pageKey,
+          child: OnBoardingPage(),
+        ),
+      ),
+      GoRoute(
+        path: "/no-connection",
+        name: "noConnection",
+        pageBuilder: (context, state) => SlideTransitionPage(
+          key: state.pageKey,
+          child: NoConnectionPage(),
+        ),
+      ),
     ],
   );
 }
