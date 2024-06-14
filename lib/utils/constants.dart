72a8b1ed56fbfa0b412c6853033a6fc7e07822c9
abel3ri <abelmerete@yahoo.com>
2024-06-15 01:08:59 +0300

    add: constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 final emailRegex = RegExp(r"^[A-Za-z0-9.+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
 final auth = FirebaseAuth.instance;
 final db = FirebaseFirestore.instance;
+BaseOptions options = BaseOptions(
+  connectTimeout: Duration(seconds: 6),
+  receiveTimeout: Duration(seconds: 6),
+);
+final dio = Dio(options);
+const BASE_URL = "https://app3.adamapress.com/wp-json/wp/v2";
