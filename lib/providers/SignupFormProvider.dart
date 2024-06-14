2671b39fb7077876aafd3bf9579a17a40f6158db
abel3ri <abelmerete@yahoo.com>
2024-06-14 17:55:22 +0300

    modify: loading spinner logic

--- lib/providers/SignupFormProvider.dart
+++ lib/providers/SignupFormProvider.dart
@@ -4,12 +4,19 @@ class SignupFormProvider extends ChangeNotifier {
   final _formKey = GlobalKey<FormState>();
 
   bool _showPassword = false;
+  bool _isLoading = false;
 
   void toggleShowPassword() {
     _showPassword = !_showPassword;
     notifyListeners();
   }
 
+  void toggleIsLoading() {
+    _isLoading = !_isLoading;
+    notifyListeners();
+  }
+
   GlobalKey<FormState> get formKey => _formKey;
   bool get showPassword => _showPassword;
+  bool get isLoading => _isLoading;
 }
