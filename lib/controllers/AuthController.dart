7f26fbd427139e7e0b3d95b1d8beba7f080b078b
abel3ri <abelmerete@yahoo.com>
2024-06-14 17:33:08 +0300

    modify: finish email,username,password auth

--- lib/controllers/AuthController.dart
+++ lib/controllers/AuthController.dart
@@ -2,6 +2,7 @@ import 'package:dalak_blog_app/models/Error.dart';
 import 'package:dalak_blog_app/models/Success.dart';
 import 'package:dalak_blog_app/utils/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fpdart/fpdart.dart';
 
 class AuthController {
@@ -27,6 +28,7 @@ class AuthController {
 
       await db.collection("users").doc(userCredential.user!.uid).set({
         "username": username,
+        "email": userCredential.user!.email,
       });
 
       return right(SuccessMessage(body: "User created successfully!"));
@@ -37,6 +39,37 @@ class AuthController {
       return left(ErrorMessage(body: e.message!));
     } catch (e) {
       await auth.currentUser!.delete();
+      return left(ErrorMessage(body: e.toString()));
+    }
+  }
+
+  static Future<Either<ErrorMessage, SuccessMessage>> loginUser({
+    required String emailUsername,
+    required String password,
+  }) async {
+    try {
+      if (emailRegex.hasMatch(emailUsername)) {
+        await auth.signInWithEmailAndPassword(
+          email: emailUsername,
+          password: password,
+        );
+      } else {
+        final res = await db
+            .collection("users")
+            .where("username", isEqualTo: emailUsername)
+            .get();
+        if (res.docs.isEmpty) {
+          return left(ErrorMessage(body: "Incorrect email or password."));
+        }
+        final email = res.docs.first.data()['email'];
+        await auth.signInWithEmailAndPassword(email: email, password: password);
+      }
+      return right(SuccessMessage(body: "Logged in successfully!"));
+    } on FirebaseAuthException catch (_) {
+      return left(ErrorMessage(body: "Incorrect email or password."));
+    } on FirebaseException catch (_) {
+      return left(ErrorMessage(body: "Unable to fetch user data"));
+    } catch (e) {
       return left(ErrorMessage(body: e.toString()));
     }
   }
