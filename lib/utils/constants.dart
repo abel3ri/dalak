07f985b7cbe58579fca64e2d81ec9d283cd98e6b
abel3ri <abelmerete@yahoo.com>
2024-06-15 23:41:01 +0300

    modify: increase timeout period

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -6,8 +6,8 @@ final emailRegex = RegExp(r"^[A-Za-z0-9.+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
 final auth = FirebaseAuth.instance;
 final db = FirebaseFirestore.instance;
 BaseOptions options = BaseOptions(
-  connectTimeout: Duration(seconds: 6),
-  receiveTimeout: Duration(seconds: 6),
+  connectTimeout: Duration(seconds: 10),
+  receiveTimeout: Duration(seconds: 10),
 );
 final dio = Dio(options);
 const BASE_URL = "https://app3.adamapress.com/wp-json/wp/v2";
