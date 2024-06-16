6808f12e3d77071368a1ac099fd463e49bd342e6
abel3ri <abelmerete@yahoo.com>
2024-06-16 19:37:39 +0300

    modify: show shimmer loading content loading

--- lib/pages/MainContentPage.dart
+++ lib/pages/MainContentPage.dart
@@ -1,4 +1,5 @@
 import 'package:dalak_blog_app/providers/ContentProvider.dart';
+import 'package:dalak_blog_app/widgets/MainShimmerLoading.dart';
 import 'package:dalak_blog_app/widgets/home/ArticleContainerXL.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
@@ -23,22 +24,29 @@ class MainContentPage extends StatelessWidget {
           : Theme.of(context).scaffoldBackgroundColor.darken(5),
       body: RefreshIndicator(
         onRefresh: () async {
+          contentProvider.toggleIsLoading();
           await contentProvider.fetchPosts(
             endpoint: "posts?categories=${categoryID}&_embed",
           );
+          contentProvider.toggleIsLoading();
         },
-        child: ListView.separated(
-          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-          itemBuilder: (context, index) {
-            return ArticleContainerXL(index: index, categoryName: categoryName);
-          },
-          separatorBuilder: (context, index) {
-            return SizedBox(
-              height: 8,
-            );
-          },
-          itemCount: contentProvider.posts.length,
-        ),
+        child: contentProvider.isLoading
+            ? MainShimmerLoading()
+            : ListView.separated(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                itemBuilder: (context, index) {
+                  return ArticleContainerXL(
+                    index: index,
+                    categoryName: categoryName,
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return SizedBox(
+                    height: 8,
+                  );
+                },
+                itemCount: contentProvider.posts.length,
+              ),
       ),
     );
   }
