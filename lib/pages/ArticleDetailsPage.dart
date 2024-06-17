0992fdc824f563140fc341ff21ed5746d3f07d54
abel3ri <abelmerete@yahoo.com>
2024-06-17 19:26:22 +0300

    modify: finsih articles details page layut

--- lib/pages/ArticleDetailsPage.dart
+++ lib/pages/ArticleDetailsPage.dart
@@ -1,3 +1,4 @@
+import 'package:dalak_blog_app/controllers/UrlController.dart';
 import 'package:dalak_blog_app/providers/ContentProvider.dart';
 import 'package:dalak_blog_app/widgets/CircleIcon.dart';
 import 'package:dalak_blog_app/widgets/CustomAppBar.dart';
@@ -6,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 class ArticlesDetailPage extends StatelessWidget {
@@ -30,112 +32,123 @@ class ArticlesDetailPage extends StatelessWidget {
           ),
           CircleIcon(
             icon: Icons.share,
-            onPressed: () {},
+            onPressed: () async {
+              await Share.shareUri(Uri.parse(contentProvider.post['link']));
+            },
           ),
         ],
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         physics: BouncingScrollPhysics(),
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(8),
-            topRight: Radius.circular(8),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    contentProvider.categoryName.toUpperCase(),
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.access_time,
-                        size: 16,
-                      ),
-                      SizedBox(width: 4),
-                      Text(timeago.format(DateTime.parse(
-                          contentProvider.posts.first['modified']))),
-                    ],
-                  ),
-                ],
-              ),
-              Html(
-                data: contentProvider.post['title']['rendered'],
-                style: {
-                  "body": Style(
-                    margin: Margins(left: Margin(0)),
-                    fontSize: FontSize.larger,
-                    fontWeight: FontWeight.bold,
-                  )
-                },
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Hero(
-                  tag: contentProvider.post['id'],
-                  child: Image.network(
-                    contentProvider
-                        .post['_embedded']['wp:featuredmedia'].first['link'],
-                    fit: BoxFit.cover,
-                  ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  contentProvider.categoryName.toUpperCase(),
+                  style: Theme.of(context).textTheme.bodyLarge,
                 ),
-              ),
-              SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: isDarkMode
-                            ? Theme.of(context)
-                                .scaffoldBackgroundColor
-                                .lighten(25)
-                            : Theme.of(context)
-                                .scaffoldBackgroundColor
-                                .darken(25),
-                        child: Icon(
-                          Icons.person,
-                          color: Colors.white,
+                Row(
+                  children: [
+                    Icon(
+                      Icons.access_time,
+                      size: 16,
+                    ),
+                    SizedBox(width: 4),
+                    Text(
+                      timeago.format(
+                        DateTime.parse(
+                          contentProvider.posts.first['modified'],
                         ),
                       ),
-                      SizedBox(width: 12),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            contentProvider
-                                .post['_embedded']['author'].first['name'],
-                          ),
-                          Text("4 min read"),
-                        ],
-                      ),
-                    ],
-                  ),
-                  FilledButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.chat_rounded),
-                    label: Text("Comments"),
-                  )
-                ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Html(
+              data: contentProvider.post['title']['rendered'],
+              style: {
+                "body": Style(
+                  margin: Margins(left: Margin(0)),
+                  fontSize: FontSize.larger,
+                  fontWeight: FontWeight.bold,
+                )
+              },
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Hero(
+                tag: contentProvider.post['id'],
+                child: Image.network(
+                  contentProvider
+                      .post['_embedded']['wp:featuredmedia'].first['link'],
+                  fit: BoxFit.cover,
+                ),
               ),
-              Html(
-                data: contentProvider.post['content']['rendered'],
-                style: {
-                  "a": Style(
-                    textDecoration: TextDecoration.none,
-                    color: Theme.of(context).colorScheme.primary,
-                  )
-                },
-              )
-            ],
-          ),
+            ),
+            SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: isDarkMode
+                          ? Theme.of(context)
+                              .scaffoldBackgroundColor
+                              .lighten(25)
+                          : Theme.of(context)
+                              .scaffoldBackgroundColor
+                              .darken(25),
+                      child: Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(width: 12),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          contentProvider
+                              .post['_embedded']['author'].first['name'],
+                        ),
+                        Text("4 min read"),
+                      ],
+                    ),
+                  ],
+                ),
+                FilledButton.icon(
+                  onPressed: () {
+                    GoRouter.of(context).pushNamed("comments");
+                  },
+                  icon: Icon(Icons.chat_rounded),
+                  label: Text("Comments"),
+                )
+              ],
+            ),
+            Html(
+              data: contentProvider.post['content']['rendered'],
+              style: {
+                "a": Style(
+                  textDecoration: TextDecoration.none,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              },
+              onAnchorTap: (url, attributes, element) async {
+                final res = await UrlController.launchUrls(url!);
+                res.fold((l) {
+                  l.showError(context);
+                }, (r) {
+                  print("Yeay!");
+                });
+              },
+            )
+          ],
         ),
       ),
     );
