3871b818b87f009862f7c9f49ecfacb92a7dad07
abel3ri <abelmerete@yahoo.com>
2024-07-13 22:34:19 +0300

    modify: fake comment fetching logic

--- lib/pages/CommentsPage.dart
+++ lib/pages/CommentsPage.dart
@@ -1,17 +1,17 @@
-// import 'package:dalak_blog_app/providers/ContentProvider.dart';
+import 'dart:async';
+
+import 'package:dalak_blog_app/utils/constants.dart';
 import 'package:dalak_blog_app/widgets/CommentInputField.dart';
 import 'package:dalak_blog_app/widgets/CommentShimmerLoading.dart';
 import 'package:dalak_blog_app/widgets/CustomAppBar.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-// import 'package:provider/provider.dart';
 
 class CommentsPage extends StatelessWidget {
   const CommentsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // final post = Provider.of<ContentProvider>(context).post;
     return Scaffold(
       bottomNavigationBar: Padding(
         padding: EdgeInsets.only(
@@ -20,7 +20,18 @@ class CommentsPage extends StatelessWidget {
           top: 12,
           bottom: MediaQuery.of(context).viewInsets.bottom,
         ),
-        child: CommentInputField(),
+        child: auth.currentUser == null
+            ? Container(
+                height: MediaQuery.of(context).size.height * 0.04,
+                child: Center(
+                    child: Text(
+                  "Login to make comments",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                )),
+              )
+            : CommentInputField(),
       ),
       appBar: CustomAppBar(
         title: "Comments",
@@ -31,7 +42,31 @@ class CommentsPage extends StatelessWidget {
           icon: Icon(Icons.close),
         ),
       ),
-      body: CommentShimmerLoading(),
+      body: FutureBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CommentShimmerLoading();
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "No comments found!",
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                        fontWeight: FontWeight.w500,
+                      ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                Text("Be the first to comment"),
+              ],
+            ),
+          );
+        },
+        future: Future.delayed(Duration(seconds: 3)),
+      ),
     );
   }
 }
